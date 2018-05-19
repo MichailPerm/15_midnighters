afc232b1f53aad17dfbb682c94131d5a7fb3d8a0
Michail_Z <apxahrejl@gmail.com>
2018-05-19 23:47:56 +0500

    Refactoring by notes

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,37 +1,43 @@
 import requests
 import pytz
-from datetime import datetime, time
+from datetime import datetime
 
 API_URL = 'https://devman.org/api/challenges/solution_attempts/'
 
 
+def get_pages_quantity():
+    response = requests.get(API_URL)
+    page_data = response.json()
+    return page_data.get('number_of_pages')
+
+
 def load_attempts():
-    pages = 2
+    pages = get_pages_quantity()
     for page in range(1, pages):
         response = requests.get(API_URL, params={'page': page})
         page_data = response.json()
-        pages = int(page_data.get('number_of_pages'))
         records = page_data.get('records')
         for record in records:
-            if record.get('timestamp'):
-                yield record
+            yield record
 
 
-def get_time(timezone, timestamp):
-    user_time = datetime.fromtimestamp(timestamp, pytz.timezone(timezone)).time()
-    if time(0, 0, 0) < user_time < time(6, 0, 0):
-        return user_time
-    return None
+def check_if_time_in_range(timezone, timestamp):
+    user_hour = datetime.fromtimestamp(timestamp, pytz.timezone(timezone)).hour
+    if 0 < user_hour < 6:
+        return True
 
 
-def get_midnighters():
+def get_midnighters(attempts):
     midnighters = []
-    for record in load_attempts():
+    for record in attempts():
         if not record:
             break
-        if get_time(record.get('timezone'), record.get('timestamp')):
+        if check_if_time_in_range(
+                record.get('timezone'),
+                record.get('timestamp')
+        ):
             midnighters.append(record.get('username'))
-
+    midnighters = sorted(list(set(midnighters)))
     return midnighters
 
 
@@ -41,5 +47,5 @@ def print_midnighters(midnighters):
 
 
 if __name__ == '__main__':
-    midnighters = get_midnighters()
+    midnighters = get_midnighters(load_attempts)
     print_midnighters(midnighters)
