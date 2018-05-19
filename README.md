07818eb574bbb691e739cb31c1c088b133a53078
Michail_Z <apxahrejl@gmail.com>
2018-05-19 12:18:31 +0500

    First release night developers seeking scritp

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+This script let user to get devman stugents, passedtheir works after midnight till 6:00
+This scritp needs reqiests and pytz packages to be installed.
+
+# Quidkstart
+
+To use this script just input in bash:
+```bash
+(lenv) user@user-desktop:~/lprojects/15_midnighters$ python seek_dev_nighters.py
+```
+
+Example of script work in Ubuntu:
+```bash
+(lenv) user@user-desktop:~/lprojects/15_midnighters$ python seek_dev_nighters.py
+id311542249
+id106957374
+id106957374
+id311542249
+
+```
+
+Executing in windows is the same.
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,17 +1,45 @@
 import requests
+import pytz
+from datetime import datetime, time
+
+API_URL = 'https://devman.org/api/challenges/solution_attempts/'
+
 
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    pages = 2
+    for page in range(1, pages):
+        response = requests.get(API_URL, params={'page': page})
+        page_data = response.json()
+        pages = int(page_data.get('number_of_pages'))
+        records = page_data.get('records')
+        for record in records:
+            if record.get('timestamp'):
+                yield record
+
+
+def get_time(timezone, timestamp):
+    user_time = datetime.fromtimestamp(timestamp, pytz.timezone(timezone)).time()
+    if time(0, 0, 0) < user_time < time(6, 0, 0):
+        return user_time
+    return None
+
 
 def get_midnighters():
-    pass
+    midnighters = []
+    for record in load_attempts():
+        if not record:
+            break
+        if get_time(record.get('timezone'), record.get('timestamp')):
+            midnighters.append(record.get('username'))
+
+    return midnighters
+
+
+def print_midnighters(midnighters):
+    for midnighter in midnighters:
+        print(midnighter)
+
 
 if __name__ == '__main__':
-  pass
+    midnighters = get_midnighters()
+    print_midnighters(midnighters)
